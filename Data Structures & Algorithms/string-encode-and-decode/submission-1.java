42e99375f3ea828a015dd690b3ed578170e500fb
Zunaid Hasan <zunaidhasan77@gmail.com>
2026-07-19 19:15:44 +0600

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String encode(List<String> strs) {
+        final String specialCharacter = "*";
+
+        StringBuilder encodedStringBuilder = new StringBuilder();
+
+        for (String string : strs) {
+            int length = string.length();
+
+            encodedStringBuilder.append(length).append(specialCharacter).append(string);
+        }
+        return encodedStringBuilder.toString();
+    }
+
+    public List<String> decode(String str) {
+
+        List<String> results = new ArrayList<>();
+
+        int i =0;
+
+        while (i < str.length()) {
+
+            int delimiterIndex = str.indexOf("*", i);
+
+            int length = Integer.parseInt(str.substring(i, delimiterIndex));
+
+            int stringStart = delimiterIndex +1;
+
+            int stringEnd = stringStart + length;
+
+            results.add(str.substring(stringStart, stringEnd));
+            i = stringEnd;
+        }
+
+        return results;
+
+    }
+}
