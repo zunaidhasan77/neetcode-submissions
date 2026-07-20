05c2d2e825a991bfe4ede67cbad373db02a397d1
Zunaid Hasan <zunaidhasan77@gmail.com>
2026-07-20 10:31:57 +0600

    Add: rotting-fruit - submission-0

---
+++ Data Structures & Algorithms/rotting-fruit/submission-0.java
@@ -0,0 +1,67 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+
+        if (grid == null || grid.length ==0) return -1;
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int freshCount =0;
+
+        int minMinutes =0;
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int r=0; r<rows; r++) {
+            for (int c=0; c< cols; c++){
+                if (grid[r][c] == 2){
+                    queue.add(new int[] {r,c});
+                }
+                if(grid[r][c] == 1) {
+                    freshCount++;
+                } 
+            }
+        }
+
+        if (freshCount ==0) return 0;
+
+        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        int minutes =0;
+
+        while (!queue.isEmpty()){
+
+            int levelSize = queue.size();
+
+            boolean rottenSomethingThisMinute = false;
+
+            for (int i =0; i< levelSize; i++) {
+                int[] current = queue.remove();
+                int row = current[0];
+                int col = current[1];
+
+                for (int[] direction : directions) {
+                    int newRow = row + direction[0];
+                    int newCol = col + direction[1];
+
+                    if (newRow <0 || newRow >= rows || newCol <0 || newCol>=cols
+                    || grid[newRow][newCol] !=1) continue;
+
+                    grid[newRow][newCol] = 2;
+                    freshCount--;
+                    rottenSomethingThisMinute = true;
+                    queue.add(new int[] {newRow, newCol});
+
+                }
+            }
+
+            if (rottenSomethingThisMinute) minutes++;
+
+        }
+        if (freshCount >0) return -1;
+
+        return minutes;
+
+        
+    }
+}
